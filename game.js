e1e6ab22ea9f6e3b29a50af1219a8fb25233ffd5
samuel <samuel2002espinoza@gmail.com>
2023-03-01 23:10:57 -0600

    He aprendido el uso de bucles bidimensionales ademas de que se consiguio ya renderizar los mapas

--- game.js
+++ game.js
@@ -9,6 +9,7 @@ window.addEventListener('resize', setCanvasSize);
 /* resize manda a llamar a este evento que es cuando se cambia de tamaño, esto con el fin de que cada que cambiemos de tamaño se ajuste al tamaño de nuestros dibujos, cuando esto se ejecuta mandamos yamar a la funcion setCanvasSize */
 
 function setCanvasSize() {
+  /* del tamaño de nuestra altura siempre y cuando sea mayor a nuestro ancho le decimos que el canvas */
   if (window.innerHeight > window.innerWidth) {
     canvasSize = window.innerWidth * 0.8;
   } else {
@@ -16,12 +17,15 @@ function setCanvasSize() {
   }
   
   canvas.setAttribute('width', canvasSize);
+  /* agregamos el tamaño que hemos dispuesto para el canvas */
   canvas.setAttribute('height', canvasSize);
+  /* setattribute borra lo que este dentro al cambiar */
   
-  elementsSize = canvasSize / 10;
+  elementsSize = ((canvasSize / 10)-1);
+  /* del tamaño del canvas lo divido entre 10 para obtener el tamaño de todo lo que tengo que poner dentro en este caso 10 bombas */
 
   startGame();
-  console.log(window.innerHeight)
+  /* llamamos a la funcion de star game, de tal forma que primero establecemos los tamaños y despues empezamos el juego */
 }
 
 function startGame() {
@@ -30,7 +34,20 @@ function startGame() {
   game.font = elementsSize + 'px Verdana';
   game.textAlign = 'end';
 
-  for (let i = 1; i <= 10; i++) {
-    game.fillText(emojis['X'], elementsSize, elementsSize * i);
+  const map = maps[1];
+  /* el metodo split nos devuelve un arreglo donde cada espacio va a hacer la separacion*/
+  /* la funcion trim nos ayuda a limpiar espacios en blanco al inicio y final */
+  const mapRow = map.trim().split('\n')
+  console.log(mapRow);
+
+  /* ahora tenemos un arreglo y no un string por ello no podemos utilizar trim */
+  /* recorro el arreglo modificando y limpiando primero los strings y luego separando letra por letra*/
+  const mapRowCol = mapRow.map(row => row.trim().split(''))
+  console.log(mapRowCol);
+
+  for (let  row = 1; row <= 10; row++) {
+    for (let col = 1; col <= 10; col++) {
+      game.fillText(emojis[mapRowCol[row-1][col-1]], elementsSize*col, ((elementsSize * row)-15));
+    }
   }
 }
