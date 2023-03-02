707d1e26c1640633791c55bf968fa2eae47e289e
samuel <samuel2002espinoza@gmail.com>
2023-03-02 12:43:30 -0600

    aprendi como utilizar los eventos y botones en js, y como vincular botones con teclas

--- game.js
+++ game.js
@@ -1,5 +1,9 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
+const btnUp = document.querySelector('#up');
+const btnLeft = document.querySelector('#left');
+const btnRight = document.querySelector('#right');
+const btnDown = document.querySelector('#down');
 
 let canvasSize;
 let elementsSize;
@@ -38,16 +42,61 @@ function startGame() {
   /* el metodo split nos devuelve un arreglo donde cada espacio va a hacer la separacion*/
   /* la funcion trim nos ayuda a limpiar espacios en blanco al inicio y final */
   const mapRow = map.trim().split('\n')
-  console.log(mapRow);
+  /* console.log(mapRow); */
 
   /* ahora tenemos un arreglo y no un string por ello no podemos utilizar trim */
   /* recorro el arreglo modificando y limpiando primero los strings y luego separando letra por letra*/
   const mapRowCol = mapRow.map(row => row.trim().split(''))
-  console.log(mapRowCol);
+  /* console.log(mapRowCol); */
 
-  for (let  row = 1; row <= 10; row++) {
+  /* forEach tambien le podemos pedir el numero del indice que se encuentra en este caso es rowI y colI */
+  mapRowCol.forEach((row, rowI) => {
+    /* es importante entender que en row lo que tenemos es un arreglo de arreglos, es decir accedemos a las filas */
+    row.forEach((col, colI) => {
+      /* en col por el contrario accedemos ya a cada elemento de cada fila y cada columna */
+      const emoji = emojis[col];
+      /* el indice empieza en 0 */
+      const posX = elementsSize * (colI+1)
+      const posY =elementsSize * (rowI+1)
+      game.fillText(emoji,posX,posY)
+      /* console.log({row,col}) */
+    });
+  });
+/*   for (let  row = 1; row <= 10; row++) {
     for (let col = 1; col <= 10; col++) {
       game.fillText(emojis[mapRowCol[row-1][col-1]], elementsSize*col, ((elementsSize * row)-15));
     }
+  } */
+}
+
+window.addEventListener('keydown',moveByKeys);
+/* keydown escucha al momento de tocar keyup al momento de haber tocado y levantar */
+btnUp.addEventListener('click', moveUp);
+btnLeft.addEventListener('click', moveLeft);
+btnRight.addEventListener('click', moveRight);
+btnDown.addEventListener('click', moveDown);
+
+function moveByKeys(event) {
+  console.log(event)
+  if (event.key == 'ArrowUp') {
+    moveUp();
+  }else if (event.key == 'ArrowLeft') {
+    moveLeft();
+  }else if (event.key == 'ArrowRight') {
+    moveRight();
+  }else if (event.key == 'ArrowDown') {
+    moveDown();
   }
+}
+function moveUp(){
+  console.log("Me movere hacia arriba");
+}
+function moveLeft(){
+  console.log("Me movere hacia Izquierda");
+}
+function moveRight(){
+  console.log("Me movere hacia derecha");
+}
+function moveDown(){
+  console.log("Me movere hacia abajo");
 }
